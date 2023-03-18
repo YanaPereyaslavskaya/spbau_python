d47b6f0fe15abbf712eeae4a71c0c3592c824605
YanaPereyaslavskaya <113440842+YanaPereyaslavskaya@users.noreply.github.com>
2023-03-18 13:35:55 +0300

    Add files via upload

--- first.py
+++ first.py
@@ -1,18 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import math
-import numpy
-import matplotlib.pyplot as mpp
+import math     # импорт библиотеки для мат операций
+import numpy    # импорт библиотеки для быстрых математических и числовых операций
+import matplotlib.pyplot as mpp     # импорт библиотеки для создания графиков
 
-#импорт библиотек
 
 # Эта программа рисует график функции, заданной выражением ниже
 
-if __name__=='__main__':
-    arguments = numpy.r_[0:200:0.1] #Значения по оси 0X с шагом 0.1
-    mpp.plot( #подсчёт графика
-        arguments,
-        [math.sin(a) * math.sin(a/20.0) for a in arguments] #заданная функция
+if __name__=='__main__':    # точка входа
+    arguments = numpy.r_[0:200:0.1]     # присваиваем переменной срез от 0 до 200 с шагом 0.1, преобразованный в строку
+    mpp.plot(   # функция создания графика из импортированного паекта
+        arguments,  # передает переменную, заданную выше
+        [math.sin(a) * math.sin(a/20.0) for a in arguments]     # функция по которой находим значения оси OY
     )
-    mpp.show() #вывод графика
+    mpp.show()     # вывод графика
